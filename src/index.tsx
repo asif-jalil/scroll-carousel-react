5a35479cde7c7ae4503f821d680c4ab4ea592aba
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2023-08-30 01:50:16 +0600

    add scroll carousel component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
 import { ScrollCarouselProps } from './interfaces/types';
+import { ScrollCarousel as ScrollCarouselInstance } from 'scroll-carousel';
+import 'scroll-carousel/dist/scroll.carousel.min.css';
 
-const ScrollCarousel: React.FC<ScrollCarouselProps> = (
-  props: ScrollCarouselProps
-) => {
-  const { speed } = props;
+const canUseDOM = !!(
+  typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement
+);
 
-  console.log(speed);
+class ScrollCarousel extends React.Component<ScrollCarouselProps> {
+  carousel: Element | NodeList | string | null;
+  scrollCarousel: ScrollCarouselInstance | null;
+  constructor(props: ScrollCarouselProps) {
+    super(props);
 
-  return <></>;
-};
+    this.carousel = null;
+    this.scrollCarousel = null;
+  }
+
+  async componentDidMount() {
+    if (!canUseDOM || !this.carousel) return null;
+    const {
+      onReady,
+      onDestroy,
+      onMove,
+      children,
+      className,
+      elementType = 'div',
+      scRef,
+      ...options
+    } = this.props;
+
+    const SC = (await import('scroll-carousel')).default;
+    this.scrollCarousel = new SC(this.carousel, {
+      ...options,
+      on: { ready: onReady, destroy: onDestroy, move: onMove },
+    });
+    if (scRef) scRef(this.scrollCarousel);
+  }
+
+  render() {
+    return React.createElement(
+      this.props.elementType || 'div',
+      {
+        className: this.props.className,
+        ref: (c) => {
+          this.carousel = c;
+        },
+      },
+      this.props.children
+    );
+  }
+}
 
 export default ScrollCarousel;
--- src/interfaces/types.tsx
+++ src/interfaces/types.tsx
@@ -1,6 +1,54 @@
 import React from 'react';
+import ScrollCarousel from 'scroll-carousel';
 
 export interface ScrollCarouselProps {
-  speed: number;
+  /**
+   * Movement speed of the carousel
+   */
+  speed?: number;
+  /**
+   * Handle the speed according to acceleration
+   */
+  smartSpeed?: boolean;
+  /**
+   * Margin between two slides
+   */
+  margin?: number;
+  /**
+   * Slide will play auto
+   */
+  autoplay?: boolean;
+  /**
+   * Speed control for autoplay
+   */
+  autoplaySpeed?: number;
+  /**
+   * select slide with class name which you want to select for carousel.
+   *  other element will behave as simple
+   */
+  slideSelector?: string;
+  /**
+   * moving direction of the slides
+   */
+  direction?: 'rtl' | 'ltr';
+
+  /**
+   * Triggered after ScrollCarousel has been activated.
+   */
+  onReady?: () => void;
+
+  /**
+   * Triggered when the slider moves.
+   */
+  onMove?: (progress: number) => void;
+
+  /**
+   * Triggered when the carousel destroyed
+   */
+  onDestroy?: () => void;
+
   children?: React.ReactNode;
+  className?: string;
+  elementType?: string;
+  scRef?: (ref: ScrollCarousel) => void;
 }
