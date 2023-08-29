d4d72b5fc57aeb2eef123f8719b5c02d86e1b2d5
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2023-08-30 01:49:51 +0600

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import terser from '@rollup/plugin-terser';
 import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -12,6 +14,7 @@ export default {
       file: pkg.main,
       name: 'ScrollCarousel',
       format: 'umd',
+      inlineDynamicImports: true,
       globals: {
         react: 'React',
       },
@@ -20,13 +23,21 @@ export default {
       file: pkg.module,
       name: 'ScrollCarousel',
       format: 'es',
+      inlineDynamicImports: true,
     },
   ],
+  context: 'this',
   plugins: [
     resolve(),
     typescript(),
     terser({ format: { comments: false }, compress: { drop_console: true } }),
-    babel({ babelHelpers: 'bundled' }),
+    babel({
+      babelHelpers: 'bundled',
+      babelrc: true,
+      exclude: 'node_modules/**',
+    }),
+    commonjs({ include: /node_modules/ }),
+    postcss(),
   ],
   external: Object.keys(pkg.peerDependencies || {}),
 };
